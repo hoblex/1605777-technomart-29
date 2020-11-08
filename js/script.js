1e19180c0880021cec21256b5668fb0cda43a223
Aleksandr Alekseev <hoblex@gmail.com>
2020-11-08 22:33:48 +0300

    Переделаны паддинги на input'ы. Отцентрированы модальные окна. Сдело всплытие "Товар в корзине" на все кнопки "Купить" на всех страницах

--- js/script.js
+++ js/script.js
@@ -126,3 +126,32 @@ buttonCloseFeedback.addEventListener("click", function (evt) {
 });
 
 
+//Обработка событий модального окна "Товар в корзине"
+
+const buttonProductToBuy = document.querySelectorAll('.link-product-to-buy');
+const displayModalGoodInCart = document.querySelector('.modal-good-in-cart');
+const buttonCloseModalGoodInCart = document.querySelector('.close-modal-good-in-cart');
+
+
+for (let i = 0; i <buttonProductToBuy.length; i++) {
+  buttonProductToBuy[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    displayModalGoodInCart.classList.add("modal-show");
+  });
+}
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (displayModalGoodInCart.classList.contains("modal-show")) {
+      evt.preventDefault();
+      displayModalGoodInCart.classList.remove("modal-show");
+    }
+  }
+});
+
+buttonCloseModalGoodInCart.addEventListener("click", function (evt) {
+  if (displayModalGoodInCart.classList.contains("modal-show")) {
+    evt.preventDefault();
+    displayModalGoodInCart.classList.remove("modal-show");
+  }
+});
