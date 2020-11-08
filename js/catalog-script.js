dd097ddeaef7ae01ac37a23f94fa70ac5fa9aca2
Aleksandr Alekseev <hoblex@gmail.com>
2020-11-08 20:16:07 +0300

    Исправлено всплытие модального окна по нажатию на кнопку "Купить". Теперь событие обрабатывается при нажатию на луюбую кнопку

--- js/catalog-script.js
+++ js/catalog-script.js
@@ -1,15 +1,16 @@
 //Обработка событий модального окна "Товар в корзине"
 
-const buttonProductToBuy = document.querySelector('.link-product-to-buy');
+const buttonProductToBuy = document.querySelectorAll('.link-product-to-buy');
 const displayModalGoodInCart = document.querySelector('.modal-good-in-cart');
 const buttonCloseModalGoodInCart = document.querySelector('.close-modal-good-in-cart');
 
 
-buttonProductToBuy.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  displayModalGoodInCart.classList.add("modal-show");
-});
-
+for (let i = 0; i <buttonProductToBuy.length; i++) {
+  buttonProductToBuy[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    displayModalGoodInCart.classList.add("modal-show");
+  });
+}
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
